93b73c1f66adecb7f99563c3f98958db6532d249
Lauren <lhanna7@ford.com>
2022-05-11 09:28:18 -0700

    generate ability score

--- dnd-character.ts
+++ dnd-character.ts
@@ -1,1 +1,37 @@
-export class DnDCharacter {}
+export class DnDCharacter {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+    hitpoints: number;
+
+    constructor() {
+        this.strength = DnDCharacter.generateAbilityScore();
+        this.dexterity = DnDCharacter.generateAbilityScore();
+        this.constitution = DnDCharacter.generateAbilityScore();
+        this.intelligence = DnDCharacter.generateAbilityScore();
+        this.wisdom = DnDCharacter.generateAbilityScore();
+        this.charisma = DnDCharacter.generateAbilityScore();
+    }
+
+    static generateAbilityScore() {
+        let topThree = this.topThreeDiceRolls
+        const sum = 0;
+        return topThree().reduce((prevValue, currValue) => prevValue + currValue, sum)
+    }
+
+    static topThreeDiceRolls() {
+        let diceRoll = []
+        while (diceRoll.length < 4) {
+            diceRoll.push(this.randomNumberGenerator())
+        }
+        const smallestRoll = Math.min(...diceRoll);
+        return diceRoll.filter(roll => roll != smallestRoll);
+    }
+
+    static randomNumberGenerator() {
+        return Math.ceil(Math.random() * 6)
+    }
+}
